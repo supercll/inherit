1ec23850f3ba8fe5e15548ffef12bb3a248bd4fd
lc <1033581609@qq.com>
2020-10-05 17:34:39 +0800

    测试集，证明Father和Son的原型互不影响

--- inherit.js
+++ inherit.js
@@ -6,9 +6,23 @@ Father.prototype.sayName = function () {
 };
 
 function Son(name, age) {
-    Father.call(Son, name);
+    Father.call(this, name);
     this.age = age;
 }
 
 Son.prototype = Object.create(Father.prototype);
 Son.prototype.constructor = Son;
+
+Son.prototype.sayAge = function () {
+    console.log(this.age);
+};
+
+let s = new Son("lc", 20);
+console.log(s);
+s.sayAge();
+s.sayName();
+
+let f = new Father("LC");
+console.log(f.sayAge); // undefined
+console.log(Son.prototype);
+console.log(Father.prototype);
